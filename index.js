2ec10a3724d40e274a80d145d17ebbd9f58a0fe1
Kyle Mitchell <kyle@kemitchell.com>
2016-01-16 00:32:55 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+module.exports = updateGoogleDriveDocument
+
+var google = require('googleapis')
+var drive = google.drive('v3')
+var https = require('https')
+var querystring = require('querystring')
+var OAuth2 = google.auth.OAuth2
+
+function updateGoogleDriveDocument(clientID, clientSecret, refreshToken, file, stream, callback) {
+  fetchAccessToken(clientID, clientSecret, refreshToken, function(error, accessToken) {
+    if (error) {
+      callback(error) }
+    else {
+      var auth = new OAuth2(clientID, clientSecret)
+      auth.setCredentials({ access_token: accessToken })
+      var request = {
+        auth: auth,
+        fileId: file,
+        resource: { mimeType: 'application/vnd.google-apps.document' },
+        media: {
+          mimeType: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+          body: stream } }
+      drive.files.update(request, callback) } }) }
+
+// https://developers.google.com/identity/protocols/OAuth2WebServer?hl=en#refresh
+function fetchAccessToken(clientID, clientSecret, refreshToken, callback) {
+  var parameters = {
+    client_id: clientID,
+    client_secret: clientSecret,
+    refresh_token: refreshToken,
+    grant_type: 'refresh_token' }
+  var body = querystring.stringify(parameters)
+  var request = {
+    host: 'www.googleapis.com',
+    path: '/oauth2/v4/token',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(body) } }
+  https.request(request, function(response) {
+    var buffers = [ ]
+    response
+      .on('data', function(buffer) {
+        buffers.push(buffer) })
+      .on('end', function() {
+        callback(null, JSON.parse(Buffer.concat(buffers)).access_token) }) })
+    .end(body) }

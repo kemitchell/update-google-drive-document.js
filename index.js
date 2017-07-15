6adff5e538c456fb29c092e5ce06c5c0d6a80027
Kyle E. Mitchell <kyle@kemitchell.com>
2017-07-15 11:14:30 -0700

    Use JavaScript Standard Style

--- index.js
+++ index.js
@@ -6,29 +6,53 @@ var https = require('https')
 var querystring = require('querystring')
 var OAuth2 = google.auth.OAuth2
 
-function updateGoogleDriveDocument(clientID, clientSecret, refreshToken, file, stream, callback) {
-  fetchAccessToken(clientID, clientSecret, refreshToken, function(error, accessToken) {
-    if (error) {
-      callback(error) }
-    else {
-      var auth = new OAuth2(clientID, clientSecret)
-      auth.setCredentials({ access_token: accessToken })
-      var request = {
-        auth: auth,
-        fileId: file,
-        resource: { mimeType: 'application/vnd.google-apps.document' },
-        media: {
-          mimeType: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-          body: stream } }
-      drive.files.update(request, callback) } }) }
+function updateGoogleDriveDocument (
+  clientID, clientSecret, refreshToken, file, stream, callback
+) {
+  fetchAccessToken(
+    clientID, clientSecret, refreshToken,
+    function (error, accessToken) {
+      if (error) {
+        callback(error)
+      } else {
+        var auth = new OAuth2(clientID, clientSecret)
+        auth.setCredentials({access_token: accessToken})
+        var request = {
+          auth: auth,
+          fileId: file,
+          resource: {
+            mimeType: 'application/vnd.google-apps.document'
+          },
+          media: {
+            mimeType: (
+              'application/' +
+              'vnd' +
+              '.openxmlformats' +
+              '-officedocument' +
+              '.wordprocessingml' +
+              '.document'
+            ),
+            body: stream
+          }
+        }
+        drive.files.update(request, callback)
+      }
+    }
+  )
+}
 
+/* eslint-disable */
 // https://developers.google.com/identity/protocols/OAuth2WebServer?hl=en#refresh
-function fetchAccessToken(clientID, clientSecret, refreshToken, callback) {
+/* eslint-enable */
+function fetchAccessToken (
+  clientID, clientSecret, refreshToken, callback
+) {
   var parameters = {
     client_id: clientID,
     client_secret: clientSecret,
     refresh_token: refreshToken,
-    grant_type: 'refresh_token' }
+    grant_type: 'refresh_token'
+  }
   var body = querystring.stringify(parameters)
   var request = {
     host: 'www.googleapis.com',
@@ -36,12 +60,18 @@ function fetchAccessToken(clientID, clientSecret, refreshToken, callback) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': Buffer.byteLength(body) } }
-  https.request(request, function(response) {
-    var buffers = [ ]
+      'Content-Length': Buffer.byteLength(body)
+    }
+  }
+  https.request(request, function (response) {
+    var buffers = []
     response
-      .on('data', function(buffer) {
-        buffers.push(buffer) })
-      .on('end', function() {
-        callback(null, JSON.parse(Buffer.concat(buffers)).access_token) }) })
-    .end(body) }
+      .on('data', function (buffer) {
+        buffers.push(buffer)
+      })
+      .on('end', function () {
+        callback(null, JSON.parse(Buffer.concat(buffers)).access_token)
+      })
+  })
+    .end(body)
+}
